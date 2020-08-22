8815ab84d2c64f28954e7e9033fadc02f6fd7378
John Stassen <john.stassen@gmail.com>
2020-08-22 15:45:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,15 @@
 def prime?(number)
   half_number = number / 2
   i = 2
+  factors = []
   while i < half_number
     if number % i == 0
-      return "Not prime"
+      factors << i
     end
     i += 1
   end
+  if factors.length = 0
+    return true
+  else
+    return false
 end
